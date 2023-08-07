8b2ffdc089e00d2faa24a1b162a03d093b036756
ATesic49 <atesic7@gmail.com>
2023-08-07 15:12:33 +0200

    NavBar

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
 import Image from "next/image";
 import styles from "../public/css/pocetna.module.css";
 export default function Home() {
-  return (
-    <main>
-      <p className={styles.xlarge}>Xlarge</p>
-      <p className={styles.large}>I</p>
-      <p className={styles.mid}>Middle</p>
-      <p className={styles.small}>Small</p>
-    </main>
-  );
+  return <main></main>;
 }
