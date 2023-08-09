b039ab424f79896423e4c32823c3c163d9ff2dd0
ATesic49 <atesic7@gmail.com>
2023-08-09 19:01:07 +0200

    a

--- app/components/Swiper.tsx
+++ app/components/Swiper.tsx
@@ -1,52 +1,58 @@
-// "use client";
-// import React from "react";
-// import { Swiper, SwiperSlide } from "swiper/react";
-// import styles from "../../public/css/swiper.module.css";
-// import "node_modules/swiper/swiper-bundle.min.css";
-// import Image from "next/image";
-// import Link from "next/link";
+"use client";
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import styles from "../../public/css/swiper.module.css";
+import "node_modules/swiper/swiper-bundle.min.css";
+import Image from "next/image";
+import Link from "next/link";
 
-// export default function Swipper() {
-//   return (
-//     <div className={styles.swiper}>
-//       <Swiper spaceBetween={0} slidesPerView={1} loop={true}>
-//         <SwiperSlide className={styles.slide}>
-//           <Image
-//             loading="lazy"
-//             src="/imgs/pocetna.jpeg"
-//             alt="Fotelja"
-//             width={500}
-//             height={300}
-//           />
-//         </SwiperSlide>
-//         <SwiperSlide className={styles.slide}>
-//           <Image
-//             loading="lazy"
-//             src="/imgs/pocetna.jpeg"
-//             alt="Fotelja"
-//             width={500}
-//             height={300}
-//           />
-//         </SwiperSlide>
-//         <SwiperSlide className={styles.slide}>
-//           <Image
-//             loading="lazy"
-//             src="/imgs/pocetna.jpeg"
-//             alt="Fotelja"
-//             width={500}
-//             height={300}
-//           />
-//         </SwiperSlide>
-//         <SwiperSlide className={styles.slide}>
-//           <Image
-//             loading="lazy"
-//             src="/imgs/pocetna.jpeg"
-//             alt="Fotelja"
-//             width={500}
-//             height={300}
-//           />
-//         </SwiperSlide>
-//       </Swiper>
-//     </div>
-//   );
-// }
+export default function Swipper() {
+  return (
+    <div className={styles.swiper}>
+      {}
+      <Swiper
+        //@ts-ignore
+        spaceBetween={0}
+        slidesPerView={1}
+        loop={true}
+      >
+        <SwiperSlide className={styles.slide}>
+          <Image
+            loading="lazy"
+            src="/imgs/pocetna.jpeg"
+            alt="Fotelja"
+            width={500}
+            height={300}
+          />
+        </SwiperSlide>
+        <SwiperSlide className={styles.slide}>
+          <Image
+            loading="lazy"
+            src="/imgs/pocetna.jpeg"
+            alt="Fotelja"
+            width={500}
+            height={300}
+          />
+        </SwiperSlide>
+        <SwiperSlide className={styles.slide}>
+          <Image
+            loading="lazy"
+            src="/imgs/pocetna.jpeg"
+            alt="Fotelja"
+            width={500}
+            height={300}
+          />
+        </SwiperSlide>
+        <SwiperSlide className={styles.slide}>
+          <Image
+            loading="lazy"
+            src="/imgs/pocetna.jpeg"
+            alt="Fotelja"
+            width={500}
+            height={300}
+          />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+}
