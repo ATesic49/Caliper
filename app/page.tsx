aa57c8c44cc133c69e9d3fe8eaeb414113ba8fd9
ATesic49 <atesic7@gmail.com>
2023-08-13 18:19:08 +0200

    ads

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import Image from "next/image";
-import Fragmenti from "./components/Fragmenti";
 import Swipper from "./components/Swiper";
 export default function Home() {
   return (
     <div>
       <Swipper />
-      <Fragmenti />
     </div>
   );
 }
