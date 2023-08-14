113e9bf5c26f2f10d12359f4009e077cb8bb6ba3
ATesic49 <atesic7@gmail.com>
2023-08-14 17:32:34 +0200

    AS

---
+++ app/loading.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "../public/css/loading.module.css";
+export default function loading() {
+  return (
+    <div className={styles.loading}>
+      <Image src={"/svgs/logo.svg"} alt="Loading" width={500} height={500} />
+      <h2>Loading...</h2>
+    </div>
+  );
+}
