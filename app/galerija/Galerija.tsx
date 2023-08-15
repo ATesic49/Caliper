680387e15430c6ea12f6fe9611ef428bd3391dcd
ATesic49 <atesic7@gmail.com>
2023-08-15 12:51:57 +0200

    rs

--- app/galerija/Galerija.tsx
+++ app/galerija/Galerija.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "../../public/css/grid.module.css";
-import "tailwindcss/tailwind.css";
 
 export default function Slike() {
   return (
