c20d830e8ecf330ec191b847e07b8e6884759727
WastedDwarf <jokicf05@gmail.com>
2023-08-15 12:46:24 +0200

    Commit2

--- app/galerija/Galerija.tsx
+++ app/galerija/Galerija.tsx
@@ -2,88 +2,230 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "../../public/css/grid.module.css";
+import "tailwindcss/tailwind.css";
 
 export default function Slike() {
   return (
-    <div className={styles.pocetak}>
-      <h1>Galerija</h1>
+    <div className={styles.galerija}>
+      <div className={styles.slika}>
+        <Image
+          src="/imgs/pocetna.jpeg"
+          alt=""
+          width={3400}
+          height={3400}
+        ></Image>
+        <h1>Najveci Izbor u Srbiji</h1>
+      </div>
       <div className={styles.grid}>
-        <div className={styles.gridItem}>
-          <Link href="/">
-            <Image src="/imgs/pocetna.jpeg" alt="" width={420} height={400} />
+        <div className={styles.container}>
+          <Link href="">
+            <Image
+              src="/imgs/pocetna.jpeg"
+              alt=""
+              width={300}
+              height={300}
+            ></Image>
           </Link>
+          <h4>Fotelja</h4>
         </div>
-        <div className={styles.gridItem}>
-          <Link href="/">
-            <Image src="/imgs/pocetna.jpeg" alt="" width={420} height={400} />
+        <div className={styles.container}>
+          <Link href="">
+            <Image
+              src="/imgs/pocetna.jpeg"
+              alt=""
+              width={300}
+              height={300}
+            ></Image>
           </Link>
+          <h4>Fotelja</h4>
         </div>
-        <div className={styles.gridItem}>
-          <Link href="/">
-            <Image src="/imgs/pocetna.jpeg" alt="" width={420} height={400} />
+        <div className={styles.container}>
+          <Link href="">
+            <Image
+              src="/imgs/pocetna.jpeg"
+              alt=""
+              width={300}
+              height={300}
+            ></Image>
           </Link>
+          <h4>Fotelja</h4>
         </div>
-        <div className={styles.gridItem}>
-          <Link href="/">
-            <Image src="/imgs/pocetna.jpeg" alt="" width={420} height={400} />
+        <div className={styles.container}>
+          <Link href="">
+            <Image
+              src="/imgs/pocetna.jpeg"
+              alt=""
+              width={300}
+              height={300}
+            ></Image>
           </Link>
+          <h4>Fotelja</h4>
         </div>
-        <div className={styles.gridItem}>
-          <Link href="/">
-            <Image src="/imgs/pocetna.jpeg" alt="" width={420} height={400} />
+        <div className={styles.container}>
+          <Link href="">
+            <Image
+              src="/imgs/pocetna.jpeg"
+              alt=""
+              width={300}
+              height={300}
+            ></Image>
           </Link>
+          <h4>Fotelja</h4>
         </div>
-        <div className={styles.gridItem}>
-          <Link href="/">
-            <Image src="/imgs/pocetna.jpeg" alt="" width={420} height={400} />
+        <div className={styles.container}>
+          <Link href="">
+            <Image
+              src="/imgs/pocetna.jpeg"
+              alt=""
+              width={300}
+              height={300}
+            ></Image>
           </Link>
+          <h4>Fotelja</h4>
         </div>
-        <div className={styles.gridItem}>
-          <Link href="/">
-            <Image src="/imgs/pocetna.jpeg" alt="" width={420} height={400} />
+        <div className={styles.container}>
+          <Link href="">
+            <Image
+              src="/imgs/pocetna.jpeg"
+              alt=""
+              width={300}
+              height={300}
+            ></Image>
           </Link>
+          <h4>Fotelja</h4>
         </div>
-        <div className={styles.gridItem}>
-          <Link href="/">
-            <Image src="/imgs/pocetna.jpeg" alt="" width={420} height={400} />
+        <div className={styles.container}>
+          <Link href="">
+            <Image
+              src="/imgs/pocetna.jpeg"
+              alt=""
+              width={300}
+              height={300}
+            ></Image>
           </Link>
+          <h4>Fotelja</h4>
         </div>
-        <div className={styles.gridItem}>
-          <Link href="/">
-            <Image src="/imgs/pocetna.jpeg" alt="" width={420} height={400} />
+        <div className={styles.container}>
+          <Link href="">
+            <Image
+              src="/imgs/pocetna.jpeg"
+              alt=""
+              width={300}
+              height={300}
+            ></Image>
           </Link>
+          <h4>Fotelja</h4>
         </div>
-        <div className={styles.gridItem}>
-          <Link href="/">
-            <Image src="/imgs/pocetna.jpeg" alt="" width={420} height={400} />
+        <div className={styles.container}>
+          <Link href="">
+            <Image
+              src="/imgs/pocetna.jpeg"
+              alt=""
+              width={300}
+              height={300}
+            ></Image>
           </Link>
+          <h4>Fotelja</h4>
         </div>
-        <div className={styles.gridItem}>
-          <Link href="/">
-            <Image src="/imgs/pocetna.jpeg" alt="" width={420} height={400} />
+        <div className={styles.container}>
+          <Link href="">
+            <Image
+              src="/imgs/pocetna.jpeg"
+              alt=""
+              width={300}
+              height={300}
+            ></Image>
           </Link>
+          <h4>Fotelja</h4>
         </div>
-        <div className={styles.gridItem}>
-          <Link href="/">
-            <Image src="/imgs/pocetna.jpeg" alt="" width={420} height={400} />
+        <div className={styles.container}>
+          <Link href="">
+            <Image
+              src="/imgs/pocetna.jpeg"
+              alt=""
+              width={300}
+              height={300}
+            ></Image>
           </Link>
+          <h4>Fotelja</h4>
         </div>
-        <div className={styles.gridItem}>
-          <Link href="/">
-            <Image src="/imgs/pocetna.jpeg" alt="" width={420} height={400} />
+        <div className={styles.container}>
+          <Link href="">
+            <Image
+              src="/imgs/pocetna.jpeg"
+              alt=""
+              width={300}
+              height={300}
+            ></Image>
           </Link>
+          <h4>Fotelja</h4>
         </div>
-        <div className={styles.gridItem}>
-          <Link href="/">
-            <Image src="/imgs/pocetna.jpeg" alt="" width={420} height={400} />
+        <div className={styles.container}>
+          <Link href="">
+            <Image
+              src="/imgs/pocetna.jpeg"
+              alt=""
+              width={300}
+              height={300}
+            ></Image>
           </Link>
+          <h4>Fotelja</h4>
         </div>
-        <div className={styles.gridItem}>
-          <Link href="/">
-            <Image src="/imgs/pocetna.jpeg" alt="" width={420} height={400} />
+        <div className={styles.container}>
+          <Link href="">
+            <Image
+              src="/imgs/pocetna.jpeg"
+              alt=""
+              width={300}
+              height={300}
+            ></Image>
           </Link>
+          <h4>Fotelja</h4>
         </div>
-        h
+        <div className={styles.container}>
+          <Link href="">
+            <Image
+              src="/imgs/pocetna.jpeg"
+              alt=""
+              width={300}
+              height={300}
+            ></Image>
+          </Link>
+          <h4>Fotelja</h4>
+        </div>
+        <div className={styles.container}>
+          <Link href="">
+            <Image
+              src="/imgs/pocetna.jpeg"
+              alt=""
+              width={300}
+              height={300}
+            ></Image>
+          </Link>
+          <h4>Fotelja</h4>
+        </div>
+        <div className={styles.container}>
+          <Link href="">
+            <Image
+              src="/imgs/pocetna.jpeg"
+              alt=""
+              width={300}
+              height={300}
+            ></Image>
+          </Link>
+          <h4>Fotelja</h4>
+        </div>
+        <div className={styles.container}>
+          <Link href="">
+            <Image
+              src="/imgs/pocetna.jpeg"
+              alt=""
+              width={300}
+              height={300}
+            ></Image>
+          </Link>
+          <h4>Fotelja</h4>
+        </div>
       </div>
     </div>
   );
