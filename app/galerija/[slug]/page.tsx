0ffa2fb7a89adc88e9ddfa3c730734a65efde91f
ATesic49 <atesic7@gmail.com>
2023-08-16 15:40:33 +0200

    dark one pro

--- app/galerija/[slug]/page.tsx
+++ app/galerija/[slug]/page.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import styles from "../../../public/css/gallery/slug/page.module.css";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 export default function page() {
-  return <div className={styles.page}></div>;
+  return (
+    <div className={styles.page}>
+      <h1>NASLOV I IME</h1>
+    </div>
+  );
 }
