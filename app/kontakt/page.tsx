8ab902e03e7b3cb7771a22fe7245b2dfeacd99bc
WastedDwarf <jokicf05@gmail.com>
2023-08-21 22:43:27 +0200

    qs

---
+++ app/kontakt/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../../public/css/kontakt.module.css";
+
+export default function Kontakt() {
+  return <div className={styles.pocetak}></div>;
+}
